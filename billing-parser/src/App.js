7760446018fc3e3474fe34e4c5e141c113d1537d
Romil <romil@avctechnologies.com>
2022-07-07 16:05:02 -0400

    Fix issues and include more data

--- billing-parser/src/App.js
+++ billing-parser/src/App.js
@@ -31,21 +31,23 @@ const useStyles = makeStyles({
     justifyContent: "flex-start",
     gridGap: "20px",
     color: "white",
+    whiteSpace: "break-spaces",
   },
   patternData: {
     display: "flex",
     flexDirection: "row",
     alignItems: "flex-start",
-    justifyContent: "space-between",
+    justifyContent: "flex-start",
     gridGap: "20px",
     color: "white",
-    width: "50%",
+    whiteSpace: "break-spaces",
   },
 });
 
 function App() {
   const cssClasses = useStyles();
   const [countryNames, setCountryNames] = useState([]);
+  const [destinationName, setDestinationName] = useState([]);
   const [countryCodes, setCountryCodes] = useState([]);
   const [cityCodes, setCityCodes] = useState([]);
   const [prices, setPrices] = useState([]);
@@ -55,34 +57,56 @@ function App() {
 
   var patternEntries = [];
 
+
+  const checkWirelessTypeFromName = (destinationString) => {
+    var concatenateStringTextArray = destinationString.toLowerCase().split(" ");
+    if(concatenateStringTextArray.some(text => text === "mobile")){
+      return true;
+    }
+    return false;
+  };
+
   const addEntries = () => {
     // Add Header Entry to CSV
-    patternEntries.push(["Country","Pattern","Amount","DisplayName"]);
+    patternEntries.push(["Country", "Pattern", "Amount", "DisplayName", "Destination"]);
     // Start
     var countryNameArray = countryNames.split(";");
+    var destinationNameArray = destinationName.split(";");
     var countryCodeArray = countryCodes.split(";");
     var cityCodeArray = cityCodes.split(";");
     var priceArray = prices.split(";");
 
     // Sort Country Entries
+    console.log(countryNameArray.length);
+    console.log(destinationNameArray.length);
+    console.log(countryCodeArray.length);
+    console.log(cityCodeArray.length);
+    console.log(priceArray.length);
     if (countryCodeArray.length === cityCodeArray.length) {
       // Make Pattern Data
       for (var i = 0; i < countryNameArray.length; i++) {
         var cityCodeSubArray = cityCodeArray[i].split(",");
+        var price = parseFloat(priceArray[i]).toFixed(4);
+        price = price.toString();
         for (var j = 0; j < cityCodeSubArray.length; j++) {
           var pattern;
-          if(cityCodeSubArray[j] === 'NA') {
-            pattern = countryCodeArray[i]
+          if (cityCodeSubArray[j] === "NA") {
+            pattern = countryCodeArray[i];
           } else {
             pattern = countryCodeArray[i] + cityCodeSubArray[j];
           }
-          var entryDisplayName =
-            displayName + " " + countryNameArray[i] + " - " + pattern;
+          var entryDisplayName;
+          if(checkWirelessTypeFromName(destinationNameArray[i])) {
+            entryDisplayName = displayName + " Wireless " + countryNameArray[i] + " - " + pattern;
+          } else {
+            entryDisplayName = displayName + " Wireline " + countryNameArray[i] + " - " + pattern;
+          }
           const entry = [
             countryNameArray[i],
             pattern,
-            priceArray[i],
+            price,
             entryDisplayName,
+            destinationNameArray[i]
           ];
           patternEntries.push(entry);
         }
@@ -104,7 +128,17 @@ function App() {
           label="Country Name"
           value={countryNames}
           onChange={(e) => {
-            setCountryNames(e.target.value.replace(/ /g, ""));
+            setCountryNames(e.target.value);
+          }}
+        />
+        <TextField
+          multiline
+          className={cssClasses.inputWidth}
+          id="destination-name"
+          label="Destination Name"
+          value={destinationName}
+          onChange={(e) => {
+            setDestinationName(e.target.value);
           }}
         />
         <TextField
@@ -143,7 +177,7 @@ function App() {
           label="Service Display Name"
           value={displayName}
           onChange={(e) => {
-            setDisplayName(e.target.value.replace(/ /g, ""));
+            setDisplayName(e.target.value);
           }}
         />
         <Button
